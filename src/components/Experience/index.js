380f44d567a35c7f64cc177fc3d6ba1d21819ecc
Alisson Matos <alissonmgsantos@gmail.com>
2020-10-25 10:50:50 -0300

    .

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import LanguageContext from '../../contexts/language';
 import { useQueryExperience } from '../../hooks/experience';
 import locale from '../../locale';
+import Image from '../Image';
 
 const Experience = () => {
   const info = useQueryExperience();
@@ -43,19 +44,7 @@ const Experience = () => {
                   <div className="flex">
                     <div className="flex-shrink-0">
                       <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                        <svg
-                          className="h-6 w-6"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor">
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                          />
-                        </svg>
+                        <Image src={item.type} width="48" height="48" />
                       </div>
                     </div>
                     <div className="ml-4">
