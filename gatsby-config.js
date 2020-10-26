24692d4c4249643e84eac36bb7b95f3745622f0d
Alisson Matos <alissonmgsantos@gmail.com>
2020-10-26 17:30:56 -0300

    Deploy github actions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: '/my_portfolio',
   siteMetadata: {
     title: `My personal Portfolio`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
