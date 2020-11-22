c201f077a7a5e3d7799038f2b643e7dacba6f044
alissonmgsantos <alissonmgsantos@gmail.com>
2020-11-22 07:56:38 -0300

    Ajuste no posicionamento do texto de sobre

--- posts/about/about_english.md
+++ posts/about/about_english.md
@@ -23,5 +23,16 @@ skills:
   - tailwindcss
   - vuejs
 bio: >-
-  Coming soon
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+          laboriosam eos accusamus quam adipisci incidunt modi est qui, labore
+          dolorem facilis aspernatur fuga perferendis, recusandae suscipit sed
+          odit nemo pariatur? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Obcaecati voluptate modi tempore blanditiis, voluptates
+          distinctio impedit aspernatur voluptas accusamus voluptatem architecto
+          quibusdam consequatur? Sint libero odio reprehenderit, ullam ut
+          Quisquam voluptatem mollitia autem quia hic culpa rerum? Lorem ipsum
+          dolor sit, amet consectetur adipisicing elit. Natus voluptatem
+          nesciunt magnam, qui quia fugit ipsa ratione consectetur ipsum libero
+          voluptas saepe excepturi! Sint accusamus molestiae, quos quaerat
+          eligendi facere. Lorem ipsum dolor sit amet consectetur adipisicing
 ---
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,39 @@
 import React, { useContext } from 'react';
 import LanguageContext from '../../contexts/language';
 import { useQueryAbout } from '../../hooks/about';
+import { useQueryHome } from '../../hooks/home';
 import locale from '../../locale';
+import Image from '../Image';
 
 const About = () => {
-  const info = useQueryAbout();
+  const info = useQueryHome();
+  const about = useQueryAbout();
   const { language } = useContext(LanguageContext);
 
   return (
     <section id="about">
-      <div id="_about">
-        <h1 className="mt-2 font-extrabold text-gray-900">
+      <div className="text-center">
+        <h1 className="m-10 font-extrabold text-gray-900">
           {locale[language].about}
         </h1>
-        <div className="flex flex-wrap pt-10">{info[language].data.bio}</div>
+      </div>
+      <div className="xs:mx-auto sm:mx-20 lg:mx-48">
+        <p class="italic text-xl" align="justify">
+          {about[language].data.bio}
+        </p>
+      </div>
+      <div className="flex flex-wrap pt-5 items-center justify-center">
+        <div className="flex flex-row items-center justify-center">
+          <a
+            href={info[language].data.linkedin}
+            target="_blank"
+            rel="noreferrer">
+            <Image src="linkedin" width="48" height="48" />
+          </a>
+          <a href={info[language].data.github} target="_blank" rel="noreferrer">
+            <Image src="github" width="48" height="48" />
+          </a>
+        </div>
       </div>
     </section>
   );
