5bc985d3a7c4ada763b751dbf2b47059f1e5c508
Alisson Matos <alissonmgsantos@gmail.com>
2021-06-06 11:38:10 -0300

    Alinhamento e remoção de código não utilizado.

--- src/components/pages/Contact/styled.js
+++ src/components/pages/Contact/styled.js
@@ -2,19 +2,14 @@ import styled from 'styled-components';
 import * as shared from '../../shared';
 
 export const Wrapper = styled(shared.Wrapper)`
-  display: flex;
   align-items: center;
-  justify-content: flex-end;
   flex-direction: column;
-  height: auto;
-  padding: 0 0 5rem 0;
 `;
 
 // Social
 export const SocialWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   margin-top: 5rem;
   @media (max-width: 767px) {
     flex-direction: column;
